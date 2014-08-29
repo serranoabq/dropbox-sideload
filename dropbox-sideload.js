0437ce2407f25bc0cedcfe15cf3e44af2b1bcdbd
serranoabq <serranoabq@gmail.com>
2014-08-29 12:57:07 -0600

    New UI

--- dropbox-sideload.js
+++ dropbox-sideload.js
@@ -1,53 +1,42 @@
+/*
+    Plugin Name: Dropbox Sideloader
+    Description: Plugin to enable sideloading media from Dropbox. 
+    Version: 0.75
+    Author: Justin R. Serrano
+*/
 // Javascript
 jQuery(document).ready(function (){ 
 	var db_options = {
-			// Required. Called when a user selects an item in the Chooser.
 			success: function(files) {
 				// each element in the files array is a file object, defined as
 				/* file = {
-					// Name of the file.
-					name: "filename.txt",
-					// URL to access the file, which varies depending on the linkType specified when the
-					// Chooser was triggered.
-					link: "https://...",
-					// Size of the file in bytes.
-					bytes: 464,
-					// URL to a 64x64px icon for the file based on the file's extension.
-					icon: "https://...",
-					// A thumbnail URL generated when the user selects images and videos.
-					// If the user didn't select an image or video, no thumbnail will be included.
-					thumbnailLink: "https://...?bounding_box=75&mode=fit",
+					name: "filename.txt", // Name of the file.
+					link: "https://...", 	// URL to the file
+					bytes: 464,						// Size of the file in bytes.
+					icon: "https://...", // URL to an icon for the file
+					thumbnailLink: "https://...?bounding_box=75&mode=fit", // URL for thumbnail with images and videos
 				} */
+					// Add file to text box
 					jQuery('#dropbox-file').val(files[0].link);
-					jQuery('#step2').attr('class','done');
-					jQuery('#step3').attr('class','done');
 					jQuery('#dropbox-sideload-button').removeAttr('disabled');
-					jQuery('#dropbox-sideload-button').prop('value','Sideload');
-					//alert("Here's the file link: " + files[0].link)
+					
+					// This is a little 'hackish', but it works at logging out the user
+					if( ! jQuery('#dropbox-stay-logged-in').prop('checked') )
+						jQuery.getScript('http://www.dropbox.com/logout');
 			},
 
-			// Optional. Called when the user closes the dialog without selecting a file
-			// and does not include any parameters.
 			cancel: function() {},
-
-			// Optional. "preview" (default) is a preview link to the document for sharing,
-			// "direct" is an expiring link to download the contents of the file. For more
-			// information about link types, see Link types below.
 			linkType: "direct",
 
-			// Optional. This is a list of file extensions. If specified, the user will
-			// only be able to select files with these extensions. You may also specify
-			// file types, such as "video" or "images" in the list. For more information,
-			// see File types below. By default, all extensions are allowed.
 		}
 	
 	jQuery('#dropbox-choose').click(function(){
 		Dropbox.choose(db_options);
 		return false;
 	});
+	
 	jQuery('#dropbox-api').on('input propertychange paste', function(){
 		jQuery('#dropbox-sideload-button').removeAttr('disabled');
-		return false;
 	});
-	//document.getElementById("dropbox-sideload-form").appendChild(button);
+
 });
--- dropbox-sideload.php
+++ dropbox-sideload.php
@@ -1,38 +1,51 @@
 <?php
 /*
     Plugin Name: Dropbox Sideloader
-    Description: Plugin to enable sideloading media from Dropbox.
-    Version: 0.1
+    Description: Plugin to enable sideloading media from Dropbox. 
+    Version: 0.75
     Author: Justin R. Serrano
-		* plugin scope: dropbox-sideload
+		Known issues: 
+			1. Sideloading is fully dependent on the WP server configuration.
+			2. The server must be able to download files from Dropbox. Some installations  
+				 have problems accessing https sites, and will not be able to download the 
+				 file locally.
+			3. Depending on the size of the file, the server operation (downloading) might
+				 time out. 
 */
-add_action('admin_init', 'dropbox_sideload_admin_init');
+
+// Initialize plugin
+add_action( 'admin_init', 'dropbox_sideload_admin_init' );
 function dropbox_sideload_admin_init(){
-	//Register our JS & CSS
-	wp_register_script ( 'dropboxjs', 'https://www.dropbox.com/static/api/2/dropins.js' );
+	// NEEDS: dropbox_sideload_add_styles()
+	// NEEDS: dropbox_sideload_menu()
+	// NEEDS: dropbox_sideload_tab_handler()
+	
+	//Register JS & CSS
+	wp_register_script ( 'dropboxjs', 
+		'https://www.dropbox.com/static/api/2/dropins.js' );
 	wp_register_script( 'dropbox-sideload', 
 		plugins_url( 'dropbox-sideload.js', __FILE__ ), array( 'dropboxjs' ) );
 	wp_register_style( 'dropbox-sideload', 
 		plugins_url('dropbox-sideload.css', __FILE__ ) );
 		
-	// NEEDS: dropbox_sideload_add_styles()
+	// Add Dropbox Sideload to Media Upload pages
 	add_action( 'load-media_page_dropbox-sideload', 'dropbox_sideload_add_styles' );
 	add_action( 'media_upload_dropbox', 'dropbox_sideload_add_styles' );
 
 	// Add a Dropbox Sideload tab to the media uploader
-	// NEEDS: dropbox_sideload_menu()
 	add_filter( 'media_upload_tabs', 'dropbox_sideload_menu' );
 	
 	// Add Dropbox Sideload tab handler
-	// NEEDS: dropbox_sideload_tab_handler()
 	add_action( 'media_upload_dropbox',  'dropbox_sideload_tab_handler' );
 	
+	// Add Dropbox API setting
 	register_setting( 'dropbox-sideload', 'dropbox-api' );
+	register_setting( 'dropbox-sideload', 'dropbox-stay-logged-in' );
 }
 
 // Assign a tab to the media uploader
-function dropbox_sideload_menu($tabs) {
-	$tabs['dropbox']='Dropbox Sideload';
+function dropbox_sideload_menu( $tabs ) {
+	$tabs['dropbox'] = 'Dropbox Sideload';
 	return $tabs;
 }
 
@@ -40,36 +53,44 @@ function dropbox_sideload_menu($tabs) {
 function dropbox_sideload_tab_handler() {
 	// NEEDS: dropbox_sideload_main_content()
 	
-	//Set the body ID
+	// Set the body ID
 	$GLOBALS['body_id'] = 'media-upload';
 	
-	//Do an IFrame header
+	// Do an IFrame header
 	iframe_header( __('Dropbox Sideload', 'dropbox-sideload') );
 	
-	//Add the Media buttons	
+	// Add the Media buttons	
 	media_upload_header();
 
-	//Do the content
+	// Do the content
 	dropbox_sideload_main_content();
 
-	//Do a footer
+	// Do a footer
 	iframe_footer();
-		
-	//return wp_iframe('dropbox_sideload_form');
 }
 
-add_action('admin_menu', 'dropbox_sideload_admin_menu');
+// Add Dropbox Sideload to the admin menus
+add_action( 'admin_menu', 'dropbox_sideload_admin_menu' );
 function dropbox_sideload_admin_menu() {
-	// NEEDS: dropbox_sideload_menu_page. 
+	// NEEDS: dropbox_sideload_menu_page()
+	
 	if ( ! function_exists('submit_button') ) return;
 	
 	if ( current_user_can('upload_files') )
-		add_media_page( __('Dropbox Sideload', 'dropbox-sideload'), __('Dropbox Sideload', 'dropbox-sideload'), 'read', 'dropbox-sideload', 'dropbox_sideload_menu_page' );
+		add_media_page( 
+			__('Dropbox Sideload', 'dropbox-sideload'),	// Page title
+			__('Dropbox Sideload', 'dropbox-sideload'),	// Menu title
+			'read', 																		// Capability
+			'dropbox-sideload', 												// Menu slug
+			'dropbox_sideload_menu_page' 								// Function callback
+		);
 	}
 
+// Handle the menu page
 function dropbox_sideload_menu_page(){
-	// NEEDS: dropbox_sideload_main_content
-	if( !current_user_can('upload_files')) return;
+	// NEEDS: dropbox_sideload_main_content()
+
+	if( ! current_user_can( 'upload_files' ) ) return;
 	
 	echo '<div class="wrap"><h2>' . __('Dropbox Sideload', 'dropbox-sideload') . '</h2>';
 
@@ -79,22 +100,42 @@ function dropbox_sideload_menu_page(){
 	echo '</div>';
 }
 
+// Create Dropbox Sideload interface
 function dropbox_sideload_main_content(){
 	// NEEDS: dropbox_sideload_handle_sideload()
-	global $pagenow;
-	$post_id = isset($_REQUEST['post_id']) ? intval($_REQUEST['post_id']) : 0;
-	$dropbox_api = get_option('dropbox-api');
+
 	
-	if (empty($dropbox_api)) {
-		if (isset($_REQUEST['dropbox-api']) && !empty($_REQUEST['dropbox-api'])){
-			$dropbox_api=$_REQUEST['dropbox-api'];
-			update_option('dropbox-api',$dropbox_api);
-		} else {
-			$dropbox_api = '';
-		}
+	global $pagenow;
+	global $wp_version;
+	
+	// Handle request parameters
+	$post_id 						= isset( $_REQUEST['post_id'] ) ? intval($_REQUEST['post_id']) : 0;
+	$dropbox_file 			= isset( $_REQUEST['dropbox-file'] ) ? $_REQUEST['dropbox-file'] : '';
+	$dropbox_api 				= isset( $_REQUEST['dropbox-api'] ) ? $_REQUEST['dropbox-api'] : '';
+	$dropbox_staylog 		= isset( $_REQUEST['dropbox-stay-logged-in'] );
+	$dropbox_delete_api = isset( $_REQUEST['dropbox-delete-api'] ) ;
+	
+	// Get options
+	$dropbox_api_option = get_option( 'dropbox-api', '' );
+	$dropbox_staylog = $dropbox_staylog || get_option( 'dropbox-stay-logged-in', false );
+	
+	if ( empty( $dropbox_api_option ) ) {
+		$dropbox_api_option = $dropbox_api; 
+		update_option( 'dropbox-api', $dropbox_api_option );
+	} else {
+		$dropbox_api = $dropbox_api_option;
 	}
-	$step1 = !empty($dropbox_api);
-	$button = __('Next', 'dropbox-sideload');
+	
+	// Clear the API Key
+	if ( $dropbox_delete_api ) {
+		$dropbox_api = '';
+		update_option( 'dropbox-api', $dropbox_api );
+	}
+	
+	// Step 1 is completed when the API setting is entered
+	$step1  = ! empty($dropbox_api);
+	$class  = $step1 ? 'step2' : 'step1';
+	$button = __( 'Next', 'dropbox-sideload' );
 	$status = '';
 	
 	if ( 'upload.php' == $pagenow ){
@@ -104,73 +145,124 @@ function dropbox_sideload_main_content(){
 		$url = admin_url('media-upload.php?tab=dropbox' );
 		$library_url = admin_url( 'media-upload.php?tab=library' );
 	}
+	
 	if ( $post_id )
 		$url = add_query_arg( 'post_id', $post_id, $url );
 
-	if( isset( $_REQUEST['dropbox-file'] ) && ! empty( $_REQUEST['dropbox-file'] ) && $step1){
-		$attachment_url = dropbox_sideload_handle_sideload( $_REQUEST['dropbox-file'] );
-		$button = __( 'Sideload', 'dropbox-sideload' );
+	if( $step1 && ! empty( $dropbox_file ) ){
+		// Step 1 & 2 completed, so handle sideload
+		$attachment_url = dropbox_sideload_handle_sideload( $dropbox_file );
+		$button = __( 'Complete', 'dropbox-sideload' );
+		$class = 'step3';
 		
+		// If successful, point the user to the Media Library
 		if ($attachment_url){
-			$status = sprintf(__('Once you have sideloaded the file, go to the <a href="%s">Media Library</a> to add it to your post.', 'dropbox-sideload'), $library_url) ;
-		}else{ 
-			$status = __('Error sideloading the file from Dropbox', 'dropbox-sideload');
+			$status = sprintf( __('The file was susccesfully sideloaded, go to the <a href="%s">Media Library</a> to add it to your post.', 'dropbox-sideload'), $library_url ) ;
+		} else { 
+			$status = __('Error sideloading the file from Dropbox. Check your logs for details.', 'dropbox-sideload');
+			$class .= ' error';
 		}
-		$step4 = true;
 	}
 	?>
+	
 	<div class="dropbox_sideload_wrap">
-	<form method="post" action="<?php echo $url ?>">
-	<ol id="dropbox-sideload-steps">
-		<li id="step1" class="<?php echo $step1?'done':'required';?>">
-			Enter your Dropbox API key:<br/>
-			<input type="text" name="dropbox-api" id="dropbox-api" value="<?php echo $dropbox_api; ?>" <?php echo $step1?'readonly':'';?>/>
-		</li>
-		<li id="step2" class="<?php echo $step1?'required':'disabled';?>">
-			<a href="#" id="dropbox-choose">Choose from Dropbox</a>
-		</li>
-		<li id="step3" class="disabled">
-			<input type="text" name="dropbox-file" id="dropbox-file" readonly />
-		</li>
-		<li id="step4" class="<?php echo $step4?'done':'disabled'?>">
-			Status: <?php echo $status; ?>
-		</li>
-	</ol>
-	<br class="clear" />
-	<?php submit_button( $button, 'primary', 'dropbox-sideload-button', false, array('disabled'=>$step1)); ?>
-	</form>
+		<?php if ( ! $step1 ) { ?>
+		<div class="updated"><p><strong>NOTE: </strong> The API key allows Dropbox Sideload to utilize the Dropbox Chooser Drop-in, but it does not provide access to any file within a Dropbox account. By selecting a file to sideload, the user grants <strong>Dropbox Sideload</strong> permission to download that file similarly as if the file were shared using Dropbox sharing features. As an added security measure, <strong>Dropbox Sideload</strong> will sign out the user of from their Dropbox account by default, unless the <code>Keep me logged-in</code> option is selected. </p></div>
+		<p>To use <strong>Dropbox Sideload</strong> you must first create a Dropbox Drop-in app. Go to the <a href="https://www.dropbox.com/developers/apps/create?app_type_checked=dropins" title="Create Dropbox Drop-in" target="_blank">Dropbox Developers site</a> to create the app. Supply a name that meets the guidelines of Dropbox. Once completed, enter your API key in the box below.</p>
+		
+		<?php } ?>
+		<form method="post" action="<?php echo $url ?>">
+		<table class="form-table <?php echo $class; ?>" id="dropbox-sideload-steps">
+			<tbody>
+				<!-- Step 1 --> 
+				<tr id="table-step1">
+					<th scope="row">
+						Dropbox API key
+					</th>
+					<td>
+						<input type="text" name="dropbox-api" id="dropbox-api" value="<?php echo $dropbox_api; ?>" <?php echo $step1 ? 'readonly' : ''; ?>/>
+						<?php if ($step1) { 
+							submit_button( __( 'Delete Key', 'dropbox-sideload' ), 'delete', 'dropbox-delete-api', false, !$step1 ? array('disabled'=>'1'): '' ); 
+						} else {
+							submit_button( __( 'Next', 'dropbox-sideload' ), 'primary', 'dropbox-continue', false, array() ); 
+						} ?>
+							<br/>
+							<span class="description"><?php _e( 'Choose a file from the Dropbox window and press Sideload', ' dropbox-sideload' ); ?> </span>
+					</td>
+				</tr>
+				<!-- Step 2 -->
+				<tr id="table-step2a">
+					<th scope="row">
+						Stay logged into Dropbox?
+					</th>
+					<td>
+						<label for="dropbox-stay-logged-in">
+							<input type="checkbox" name="dropbox-stay-logged-in" id="dropbox-stay-logged-in" <?php echo $dropbox_staylog ? 'checked' : '' ; ?> > Keep me logged-in after choosing file
+						</label>
+					</td>
+				</tr>
+				<tr id="table-step2b">
+					<th scope="row">
+						Choose sideload file
+					</th>
+					<td>
+						<input type="text" name="dropbox-file" id="dropbox-file" value="<?php echo $dropbox_file; ?>" readonly /> 
+						<a href="#" id="dropbox-choose" class="button <?php echo ! $step1 ? 'disabled' : ''; ?>">Choose from Dropbox</a>
+						<?php submit_button( __( 'Sideload', 'dropbox-sideload' ), 'primary', 'dropbox-sideload-button', false, array('disabled' => 'disabled') ); ?>
+						<br/>
+						<span class="description"><?php _e( 'Enter your Dropbox API Key and press Next', ' dropbox-sideload' ); ?> </span>
+					</td>
+				</tr>
+				<!-- Step 3? -->
+				<tr id="table-step3">
+					<th scope="row">
+						Sideload stauts
+					</th>
+					<td>
+						<?php echo $status; ?>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<?php /*
+		<ol id="dropbox-sideload-steps">
+			<li id="step1" class="<?php echo $step1?'done':'required';?>">
+				<strong>Enter your Dropbox Drop-ins API key.</strong></br>
+				
+			</li>
+			<li id="step2" class="<?php echo $step1?'required':'disabled';?>">
+				<strong>Choose from Dropbox</strong></br>
+				<label for="dropbox-stay-logged-in">
+					<input type="checkbox" name="dropbox-stay-logged-in" id="dropbox-stay-logged-in" <?php echo $dropbox_staylog ? 'checked' : '' ; ?> > Keep me logged-in after choosing file
+				</label><br/>
+				
+			</li>
+			<li id="step3" class="<?php echo $step3?'done':'disabled'?>">
+				Status: <?php echo $status; ?>
+			</li>
+		</ol>
+		*/?>
+		<br class="clear" />
+		<?php //submit_button( $button, 'primary', 'dropbox-sideload-button', false, array( 'disabled' => $step1 ) ); ?>
+		</form>
 	</div>
 <?php
 }
 
-// There's no easy way to add the id and app-key so hijack the clean_url filter
-add_filter('clean_url','unclean_url',10,3);
-function unclean_url( $good_protocol_url, $original_url, $_context){
-	
-	$dropbox_api = get_option('dropbox-api');
-	if (empty($dropbox_api)) return $original_url;
-	if (false !== strpos($original_url, 'dropbox.com')){
-		remove_filter('clean_url','unclean_url',10,3);
-      $url_parts = parse_url($good_protocol_url);
-      return $url_parts['scheme'] . '://' . $url_parts['host'] . $url_parts['path'] . "' id='dropboxjs' data-app-key='".$dropbox_api;
-	}
-	return $good_protocol_url;
-}
-
-//Add scripts & styles
+// Add scripts & styles
 function dropbox_sideload_add_styles() {	
 	if ( 'media_upload_server' == current_filter() )
 			wp_enqueue_style('media');
 			
-	wp_enqueue_script('dropboxjs' );
-	wp_enqueue_script('dropbox-sideload');
-	wp_enqueue_style('dropbox-sideload');
+	wp_enqueue_script( 'dropboxjs' );
+	wp_enqueue_script( 'dropbox-sideload' );
+	wp_enqueue_style( 'dropbox-sideload' );
 }
 
 
-// Handle the sideloading. Returns attachment url
+// Handle the sideloading. Returns attachment url on success, FALSE on failure
 function dropbox_sideload_handle_sideload($url){
-	
+	// Download file to temporary location
 	$tmp = download_url( $url );
 	$file_array = array(
 		'name' => basename( $url ),
@@ -179,116 +271,40 @@ function dropbox_sideload_handle_sideload($url){
 	
 	// Check for download errors
 	if ( is_wp_error( $tmp ) ) {
+		error_log( $tmp -> get_error_message() );
 		@unlink( $file_array[ 'tmp_name' ] );
 		return false;
 	}
+	
+	// Do the sideloading into WP
 	$id = media_handle_sideload( $file_array, 0);
 	
 	// Check for handle sideload errors.
 	if ( is_wp_error( $id ) ) {
+		error_log( $id -> get_error_message() );
 		@unlink( $file_array['tmp_name'] );
 		return false;
 	}
+	
+	// Success! So get the attachment url as a sign of success
 	$attachment_url = wp_get_attachment_url( $id );
 		
 	return $attachment_url;
 }
 
-/*
-function dropbox_sideload_form () {
-	global $pagenow;
-	$post_id = isset($_REQUEST['post_id']) ? intval($_REQUEST['post_id']) : 0;
-	$url = admin_url('media-upload.php?tab=dropbox');
-
-	if ( $post_id )
-		$url = add_query_arg('post_id', $post_id, $url);
-<<<<<<< HEAD
+// Dropbox Chooser requires the API key to be added to the script call
+add_filter('clean_url','unclean_url',10,3);
+function unclean_url( $good_protocol_url, $original_url, $_context){
 	
-	if( isset( $_REQUEST['dropbox-file'] ) ){
-		$dpurl = $_REQUEST['dropbox-file']; // Input a .zip URL here
-		$tmp = download_url( $dpurl );
-		
-		$file_array = array(
-				'name' => basename( $dpurl ),
-				'tmp_name' => $tmp
-		);
-		
-		// Check for download errors
-		if ( is_wp_error( $tmp ) ) {
-				@unlink( $file_array[ 'tmp_name' ] );
-				echo '<div class="ds_error">Error downloading the file from Dropbox</div>';
-				return $tmp;
-		}
-
-		$id = media_handle_sideload( $file_array, $post_id );
-		// Check for handle sideload errors.
-		if ( is_wp_error( $id ) ) {
-			echo '<div class="ds_error">Error sideloading the file from Dropbox</div>';
-			@unlink( $file_array['tmp_name'] );
-			return $id;
-		}
-
-		$attachment_url = wp_get_attachment_url( $id );
-		// Do whatever you have to here
-		echo '<div class="success">';
-		echo '<h3>File Downloaded Succesfully!</h3>';
-		echo 'The file: <br/>';
-		echo '<code>'.$file_array[ 'name' ] .'</code></br>';
-		echo 'was downloaded to</br>';
-		echo '<code>'.$attachment_url.'</code></br>';
-		echo 'Press the <b>Insert Into Post</b> button';
-		echo '</div>';
-		
-	} else {
-=======
-	if ( isset( $_REQUEST['dropbox-file']) ){
-		$dpurl = $_REQUEST['dropbox-file'];
-		$tmp = download_url( $dpurl );
-		$file_array = array(
-			'name' => basename( $dpurl ),
-			'tmp_name' => $tmp
-		);
-
-		// Check for download errors
-		if ( is_wp_error( $tmp ) ) {
-			@unlink( $file_array[ 'tmp_name' ] );
-			return $tmp;
-		}
-		$id = media_handle_sideload( $file_array, $post_id );
-		// Check for handle sideload errors.
-		if ( is_wp_error( $id ) ) {
-			@unlink( $file_array['tmp_name'] );
-			return $id;
-		}
-		$attachment_url = wp_get_attachment_url( $id );
-		echo '<script>jQuery(document).readd(function($){
-			parent.wp.media.editor.insert('.$attachment_url.');
-			return false;
-			</script>';
-	} else {
-		media_upload_header();
-		dropbox_sideload_scripts();
->>>>>>> origin/master
+	$dropbox_api = get_option( 'dropbox-api' , '' );
+	if ( empty( $dropbox_api ) ) return $original_url;
 	
-	?>
-	<div id="dropbox-sideload-form">
-		<form method="post" action="<?php echo $url; ?>">
-		<input type="hidden" name="post_id" id="<?php echo $post_id; ?>" value="0" />
-		<?php wp_nonce_field(); ?>
-		<div id="media-upload-notice"></div>
-		<div id="media-upload-error"></div>
-		<h3 class="media-title">Upload files from your Dropbox</h3>
-		<ol id="dropbox-sideload-steps">
-		<li id="step1"><a href="#" id="dropbox-choose">Choose from Dropbox</a></li>
-		<li id="step2"><input type="text" name="dropbox-file" id="dropbox-file" readonly /></li>
-		<li id="step3"><input type="submit" class="button media-button" id="dropbox-form-submit" name="dropbox-form-submit" disabled="disabled"/></li>
-		</ol>
-	</div> 	
-
-<?php }
+	if (false !== strpos( $original_url, 'dropbox.com' ) ) {
+		remove_filter( 'clean_url' , 'unclean_url' , 10, 3 );
+		$url_parts = parse_url( $good_protocol_url );
+		return $url_parts['scheme'] . '://' . $url_parts['host'] . $url_parts['path'] . "' id='dropboxjs' data-app-key='".$dropbox_api;
 	}
+	return $good_protocol_url;
+}
 
-
-
-*/
 ?>
